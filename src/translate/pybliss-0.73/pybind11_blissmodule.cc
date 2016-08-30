1bc304f2ebb27fd11afa86b22a6b9fe86a8b64cc
Silvan Sievers <silvan.sievers@unibas.ch>
2016-08-30 17:28:55 +0200

    [FO-symmetries] use distutils to setup also the pybind extension. use a 'better' setup.py version for pyext bliss module

--- src/translate/pybliss-0.73/pybind11_blissmodule.cc
+++ src/translate/pybliss-0.73/pybind11_blissmodule.cc
@@ -6,8 +6,8 @@ using namespace std;
 using namespace pybind11::literals;
 namespace py = pybind11;
 
-PYBIND11_PLUGIN(bliss_wrapper) {
-    py::module m("bliss_wrapper", "pybind11 bliss plugin");
+PYBIND11_PLUGIN(pybind11_blissmodule) {
+    py::module m("pybind11_blissmodule", "pybind11 bliss module");
 
     py::class_<DigraphWrapper>(m, "DigraphWrapper")
         .def(py::init<>())
@@ -15,9 +15,7 @@ PYBIND11_PLUGIN(bliss_wrapper) {
             "color"_a)
         .def("add_edge", &DigraphWrapper::add_edge, "doc",
             "v1"_a, "v2"_a)
-        .def("find_automorphisms", &DigraphWrapper::find_automorphisms)
-        .def("add_automorphism", &DigraphWrapper::add_automorphism)
-        .def("get_automorphisms", &DigraphWrapper::get_automorphisms);
+        .def("find_automorphisms", &DigraphWrapper::find_automorphisms);
 
     return m.ptr();
 }
