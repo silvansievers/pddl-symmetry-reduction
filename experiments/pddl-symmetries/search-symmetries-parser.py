492049a1a44ec5af59c977b3a3a6051018cdc59b
Silvan Sievers <silvan.sievers@unibas.ch>
2017-02-16 12:02:48 +0100

    [FO-symmetries] run search with lifted and ground symmetries

--- experiments/pddl-symmetries/search-symmetries-parser.py
+++ experiments/pddl-symmetries/search-symmetries-parser.py
@@ -5,5 +5,6 @@ from lab.parser import Parser
 parser = Parser()
 parser.add_pattern('generators_count', 'Number of generators: (\d+)', required=False, type=int)
 parser.add_pattern('generators_orders', 'Order of generators: \[(.*)\]', required=False, type=str)
+parser.add_pattern('time_symmetries', 'Done initializing symmetries: (.+)s', required=False, type=float)
 
 parser.parse()
