9b875f6ef223e99722a2174adfe79cb265c2cb8d
Silvan Sievers <silvan.sievers@unibas.ch>
2017-10-10 17:36:42 +0200

    [FO-symmetries] fix report

--- experiments/pddl-symmetries/2017-10-10-lifted-stabinit-baggy.py
+++ experiments/pddl-symmetries/2017-10-10-lifted-stabinit-baggy.py
@@ -257,10 +257,10 @@ def main(revisions=None):
         return props
 
     exp.add_absolute_report_step(attributes=attributes,filter_algorithm=[
-        '{}-translate'.format(REVISION),
-        '{}-translate-stabinit'.format(REVISION),
-        #'{}-translate-stabinit-ground'.format(REVISION),
-        #'{}-translate-stabinit-ground-noneofthose'.format(REVISION),
+        '{}-baggy-translate'.format(REVISION),
+        '{}-baggy-translate-stabinit'.format(REVISION),
+        #'{}-baggy-translate-stabinit-ground'.format(REVISION),
+        #'{}-baggy-translate-stabinit-ground-noneofthose'.format(REVISION),
     ],filter=[compute_removed_count_in_each_step])
 
     exp.run_steps()
