0ff6d63975e0c18d864f19599699fbcecb469eee
Silvan Sievers <silvan.sievers@unibas.ch>
2018-06-18 15:13:13 +0200

    [FO-symmetries] use fix

--- experiments/pddl-symmetries/2018-06-18-lifted-stabinit-order.py
+++ experiments/pddl-symmetries/2018-06-18-lifted-stabinit-order.py
@@ -17,7 +17,7 @@ except ImportError:
     print 'matplotlib not available, scatter plots not available'
     matplotlib = False
 
-REVISION = '2d2dd3a9ba28'
+REVISION = '60f9d60b4f7c'
 
 def main(revisions=None):
     benchmarks_dir=os.path.expanduser('~/repos/downward/benchmarks')
