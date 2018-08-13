cf4e1deffec2cb696f5024c465131b7ffdbc17be
Silvan Sievers <silvan.sievers@unibas.ch>
2018-08-13 11:18:02 +0200

    [FO-symmetries] rerun lifted exp without computing orders for time measurements

---
+++ experiments/pddl-symmetries/2018-08-13-baggy-lifted-no-order.py
@@ -0,0 +1,207 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import suites
+
+from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+from lab.reports import Attribute, geometric_mean
+from downward.reports.absolute import AbsoluteReport
+from downward.reports.compare import ComparativeReport
+
+from common_setup import IssueConfig, IssueExperiment, DEFAULT_OPTIMAL_SUITE, is_test_run
+try:
+    from relativescatter import RelativeScatterPlotReport
+    matplotlib = True
+except ImportError:
+    print 'matplotlib not available, scatter plots not available'
+    matplotlib = False
+
+REVISION = 'ef5ac3aa3935'
+
+def main(revisions=None):
+    benchmarks_dir=os.path.expanduser('~/repos/downward/baggy-benchmarks')
+    # optimal union satisficing,
+    suite = [
+        #'openstacks-sat08-adl',
+        'miconic-simpleadl',
+        'barman-sat14-strips',
+        'transport-opt11-strips',
+        'openstacks-sat08-strips',
+        'logistics98',
+        'parking-sat11-strips',
+        #'psr-large',
+        'rovers',
+        'floortile-opt14-strips',
+        'barman-opt14-strips',
+        'zenotravel',
+        'elevators-sat11-strips',
+        'nomystery-opt11-strips',
+        'parcprinter-08-strips',
+        #'tidybot-opt11-strips',
+        'cavediving-14-adl',
+        'pegsol-opt11-strips',
+        'maintenance-opt14-adl',
+        'citycar-opt14-adl',
+        'pipesworld-notankage',
+        'woodworking-sat08-strips',
+        'woodworking-opt11-strips',
+        'driverlog',
+        'gripper',
+        'visitall-sat11-strips',
+        #'openstacks',
+        'hiking-opt14-strips',
+        'sokoban-opt11-strips',
+        'tetris-sat14-strips',
+        'parcprinter-opt11-strips',
+        'openstacks-strips',
+        'parcprinter-sat11-strips',
+        'grid',
+        'sokoban-opt08-strips',
+        'elevators-opt08-strips',
+        'openstacks-sat14-strips',
+        'barman-sat11-strips',
+        #'tidybot-sat11-strips',
+        'mystery',
+        'visitall-opt14-strips',
+        'childsnack-sat14-strips',
+        'sokoban-sat11-strips',
+        #'trucks',
+        'sokoban-sat08-strips',
+        'barman-opt11-strips',
+        'childsnack-opt14-strips',
+        'parking-opt14-strips',
+        'openstacks-opt11-strips',
+        'elevators-sat08-strips',
+        #'movie',
+        #'tidybot-opt14-strips',
+        #'freecell',
+        'openstacks-opt14-strips',
+        'scanalyzer-sat11-strips',
+        #'ged-opt14-strips',
+        'pegsol-sat11-strips',
+        'transport-opt08-strips',
+        #'mprime',
+        'floortile-opt11-strips',
+        'transport-sat08-strips',
+        'pegsol-08-strips',
+        #'blocks',
+        'floortile-sat11-strips',
+        'thoughtful-sat14-strips',
+        'openstacks-opt08-strips',
+        'visitall-sat14-strips',
+        'pipesworld-tankage',
+        'scanalyzer-opt11-strips',
+        'storage',
+        'maintenance-sat14-adl',
+        #'optical-telegraphs',
+        'elevators-opt11-strips',
+        'miconic',
+        'logistics00',
+        'depot',
+        'transport-sat11-strips',
+        #'openstacks-opt08-adl',
+        'psr-small',
+        'satellite',
+        #'assembly',
+        'citycar-sat14-adl',
+        'schedule',
+        #'miconic-fulladl',
+        'pathways-noneg',
+        'tetris-opt14-strips',
+        #'ged-sat14-strips',
+        'pathways',
+        'woodworking-opt08-strips',
+        'floortile-sat14-strips',
+        'nomystery-sat11-strips',
+        'transport-opt14-strips',
+        'woodworking-sat11-strips',
+        #'philosophers',
+        'trucks-strips',
+        'hiking-sat14-strips',
+        'transport-sat14-strips',
+        'openstacks-sat11-strips',
+        'scanalyzer-08-strips',
+        'visitall-opt11-strips',
+        #'psr-middle',
+        'airport',
+        'parking-opt11-strips',
+        'tpp',
+        'parking-sat14-strips',
+    ]
+
+    environment = BaselSlurmEnvironment(email="silvan.sievers@unibas.ch", export=["PATH"])
+
+    if is_test_run():
+        suite = ['gripper:prob01.pddl', 'depot:p01.pddl', 'mystery:prob07.pddl', 'miconic-simpleadl:s1-0.pddl']
+        environment = LocalEnvironment(processes=4)
+
+    configs = {
+        IssueConfig('baggy-translate', [], driver_options=['--translate', '--translate-time-limit', '30m', '--translate-memory-limit', '3584M']),
+        IssueConfig('baggy-translate-symm-stabgoal-stabinit', ['--translate-options', '--compute-symmetries', '--bliss-time-limit', '300', '--stop-after-computing-symmetries'], driver_options=['--translate', '--translate-time-limit', '30m', '--translate-memory-limit', '3584M']),
+        IssueConfig('baggy-translate-symm-stabinit', ['--translate-options', '--compute-symmetries', '--do-not-stabilize-goal', '--bliss-time-limit', '300', '--stop-after-computing-symmetries'], driver_options=['--translate', '--translate-time-limit', '30m', '--translate-memory-limit', '3584M']),
+        IssueConfig('baggy-translate-symm-stabgoal', ['--translate-options', '--compute-symmetries', '--do-not-stabilize-initial-state', '--bliss-time-limit', '300', '--stop-after-computing-symmetries'], driver_options=['--translate', '--translate-time-limit', '30m', '--translate-memory-limit', '3584M']),
+        IssueConfig('baggy-translate-symm', ['--translate-options', '--compute-symmetries', '--do-not-stabilize-goal', '--do-not-stabilize-initial-state', '--bliss-time-limit', '300', '--stop-after-computing-symmetries'], driver_options=['--translate', '--translate-time-limit', '30m', '--translate-memory-limit', '3584M']),
+    }
+
+    exp = IssueExperiment(
+        revisions=revisions,
+        configs=configs,
+        environment=environment,
+    )
+    exp.add_suite(benchmarks_dir, suite)
+
+    exp.add_parser(exp.EXITCODE_PARSER)
+    exp.add_parser(exp.TRANSLATOR_PARSER)
+    exp.add_parser('symmetries-parser.py')
+
+    generator_count_lifted = Attribute('generator_count_lifted', absolute=True, min_wins=False)
+    time_symmetries1_symmetry_graph = Attribute('time_symmetries1_symmetry_graph', absolute=False, min_wins=True, functions=[geometric_mean])
+    time_symmetries2_bliss = Attribute('time_symmetries2_bliss', absolute=False, min_wins=True, functions=[geometric_mean])
+    bliss_out_of_memory = Attribute('bliss_out_of_memory', absolute=True, min_wins=True)
+    bliss_out_of_time = Attribute('bliss_out_of_time', absolute=True, min_wins=True)
+    translator_completed = Attribute('translator_completed', absolute=True, min_wins=False)
+    timeout = Attribute('timeout', absolute=True, min_wins=True)
+    symmetry_graph_size = Attribute('symmetry_graph_size', absolute=True, min_wins=True)
+    time_symmetries = Attribute('time_symmetries', absolute=False, min_wins=True, functions=[geometric_mean])
+    symmetry_group_order = Attribute('symmetry_group_order', absolute=True, min_wins=False)
+    symmetry_group_order_time = Attribute('symmetry_group_order_time', absolute=False, min_wins=False, functions=[geometric_mean])
+
+    extra_attributes = [
+        generator_count_lifted,
+        time_symmetries1_symmetry_graph,
+        time_symmetries2_bliss,
+        bliss_out_of_memory,
+        bliss_out_of_time,
+        translator_completed,
+        timeout,
+        symmetry_graph_size,
+        time_symmetries,
+        symmetry_group_order,
+        symmetry_group_order_time,
+    ]
+    attributes = ['error', 'run_dir'] # exp.DEFAULT_TABLE_ATTRIBUTES
+    attributes.extend(extra_attributes)
+    attributes.append('translator_time_symmetries*')
+
+    exp.add_step('build', exp.build)
+    exp.add_step('start', exp.start_runs)
+    exp.add_fetcher(name='fetch')
+
+    def duplicate_attribute(props):
+        props['time_symmetries'] = props.get('translator_time_symmetries0_computing_symmetries', None)
+        return props
+
+    algorithm_nicks = [
+        'baggy-translate',
+        'baggy-translate-symm-stabgoal-stabinit',
+        'baggy-translate-symm-stabinit',
+        'baggy-translate-symm-stabgoal',
+        'baggy-translate-symm',
+    ]
+
+    exp.add_absolute_report_step(attributes=attributes,filter_algorithm=['{}-{}'.format(REVISION, x) for x in algorithm_nicks],filter=[duplicate_attribute])
+
+    exp.run_steps()
+
+main(revisions=[REVISION])
