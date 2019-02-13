bf1917869e290bf5776b76ec4e82431be193fe6d
Gabi Roeger <gabriele.roeger@unibas.ch>
2019-02-13 23:11:52 +0100

    [abstract-structures] removed outdated code

--- src/translate/find_symmetries.py
+++ src/translate/find_symmetries.py
@@ -26,22 +26,4 @@ if __name__ == "__main__":
     f = open('out.dot', 'w')
     write_dot_graph(graph, vertex_no_to_structure, f)
     f.close()
-
-#    only_object_symmetries = options.only_object_symmetries
-#    stabilize_initial_state = not options.do_not_stabilize_initial_state
-#    stabilize_goal = not options.do_not_stabilize_goal
-#    time_limit = options.bliss_time_limit
-#    write_group_generators = options.write_group_generators
-#    add_object_type_nodes = options.add_object_type_nodes
-#    task = pddl_parser.open()
-#    normalize.normalize(task)
-#    task.dump()
-#    graph = SymmetryGraph(task, only_object_symmetries, stabilize_initial_state, stabilize_goal, add_object_type_nodes)
-#    if WRITE_DOT_GRAPH:
-#        f = open('out.dot', 'w')
-#        graph.write_dot_graph(f, hide_equal_predicates=True)
-#        f.close()
-#    automorphisms = graph.find_automorphisms(time_limit, write_group_generators)
-#    graph.write_or_print_automorphisms(automorphisms, hide_equal_predicates=True, write=False, dump=True)
-#    sys.stdout.flush()
-
+    sys.stdout.flush()
