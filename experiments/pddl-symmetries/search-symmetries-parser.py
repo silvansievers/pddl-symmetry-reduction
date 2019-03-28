e4e3372f4d3e8f433364127fc19387743efd0b86
Silvan Sievers <silvan.sievers@unibas.ch>
2019-03-28 09:39:41 +0100

    [abstract-structures] adapt parsers according to new output

--- experiments/pddl-symmetries/search-symmetries-parser.py
+++ experiments/pddl-symmetries/search-symmetries-parser.py
@@ -3,9 +3,9 @@
 from lab.parser import Parser
 
 parser = Parser()
-parser.add_pattern('num_search_generators', 'Number of search generators \(affecting facts\): (\d+)', required=False, type=int)
-parser.add_pattern('num_operator_generators', 'Number of identity generators \(on facts, not on operators\): (\d+)', required=False, type=int)
-parser.add_pattern('num_total_generators', 'Total number of generators: (\d+)', required=False, type=int)
+parser.add_pattern('num_ground_search_generators', 'Number of search generators \(affecting facts\): (\d+)', required=False, type=int)
+parser.add_pattern('num_ground_operator_generators', 'Number of identity generators \(on facts, not on operators\): (\d+)', required=False, type=int)
+parser.add_pattern('num_ground_generators', 'Total number of generators: (\d+)', required=False, type=int)
 parser.add_pattern('symmetry_graph_size', 'Size of the grounded symmetry graph: (\d+)', required=False, type=int)
 parser.add_pattern('time_symmetries', 'Done initializing symmetries: (.+)s', required=False, type=float)
 parser.add_pattern('symmetry_group_order', 'Symmetry group order: (\d+)', required=False, type=int)
--- experiments/pddl-symmetries/symmetries-parser.py
+++ experiments/pddl-symmetries/symmetries-parser.py
@@ -5,10 +5,10 @@ import re
 from lab.parser import Parser
 
 parser = Parser()
-parser.add_pattern('generator_count_lifted', 'Number of raw generators: (\d+)', required=False, type=int)
-parser.add_pattern('generator_count_lifted_interesting', 'Number of interesting generators: (\d+)', required=False, type=int)
+parser.add_pattern('num_lifted_generators', 'Number of generators: (\d+)', required=False, type=int)
 parser.add_pattern('time_symmetries1_symmetry_graph', 'Done creating abstract structure graph: (.+)s', required=False, type=float)
 parser.add_pattern('time_symmetries2_bliss', 'Done searching for generators: (.+)s', required=False, type=float)
+parser.add_pattern('time_symmetries3_translate', 'Done translating generators: (.+)s', required=False, type=float)
 parser.add_pattern('symmetry_graph_size', 'Size of abstract structure graph: (\d+)', required=False, type=int)
 parser.add_pattern('symmetry_group_order', 'Symmetry group order: (\d+)', required=False, type=int)
 
@@ -24,13 +24,6 @@ parser.add_function(add_composed_attributes)
 def parse_boolean_flags(content, props):
     bliss_memory_out = False
     bliss_timeout = False
-    generator_lifted_affecting_actions_axioms = False
-    generator_lifted_mapping_actions_axioms = False
-    generator_not_well_defined_for_search = False
-    ignore_none_of_those_mapping = False
-    simplify_var_removed = False
-    simplify_val_removed = False
-    reorder_var_removed = False
     lines = content.split('\n')
     for line in lines:
         if 'Bliss memory out' in line:
@@ -43,16 +36,6 @@ def parse_boolean_flags(content, props):
     props['bliss_out_of_time'] = bliss_timeout
 
 parser.add_function(parse_boolean_flags)
-
-def parse_memory_error(content, props):
-    translate_out_of_memory = False
-    lines = content.split('\n')
-    for line in lines:
-        if line == 'MemoryError':
-            translate_out_of_memory = True
-    props['translate_out_of_memory'] = translate_out_of_memory
-
-parser.add_function(parse_memory_error, file='run.err')
 
 def duplicate_attribute(content, props):
     props['time_symmetries'] = props.get('translator_time_symmetries_computing_symmetries', None)
